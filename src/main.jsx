a92af95b206bd28f9bb099283f45c8ce07b5d6a3
Mohamed Omar <mohamed.omar.mubark@gmail.com>
2024-07-13 00:20:46 +0300

    style: :lipstick: add home page layout

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,8 @@ import "primeflex/primeflex.css";
 import "primeicons/primeicons.css";
 
 // styles
-import "./assets/styles/main.scss";
+import "./assets/styles/app.scss";
+import "./assets/styles/home.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,20 @@ import React from "react";
 
 const Home = () => {
   return (
-    <>
-      <h1>test</h1>
-    </>
+    <div className="home">
+      <div className="header p-4 bg-white">
+        <div className="container">Header</div>
+      </div>
+      <div className="container layout">
+        <div className="main">
+          <div className="profile bg-green-200">Profile</div>
+
+          <div className="posts bg-red-200">Posts</div>
+
+          <div className="side bg-orange-200">Side</div>
+        </div>
+      </div>
+    </div>
   );
 };
 
