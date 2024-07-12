717af29e217b80576716474cd9eefb4568ec56ca
Mohamed Omar <mohamed.omar.mubark@gmail.com>
2024-07-12 23:21:00 +0300

    build: :heavy_plus_sign: install Prime react, flex, icons

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
+// primereact
+import "primereact/resources/themes/lara-light-teal/theme.css";
+// primeflex
+import "primeflex/primeflex.css";
+// primeicons
+import "primeicons/primeicons.css";
+
 // styles
-import "./assets/styles/main.css";
+import "./assets/styles/main.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
