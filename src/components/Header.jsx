de7904e0884b42a9d7230993d7014bead4adc070
Mohamed Omar <mohamed.omar.mubark@gmail.com>
2024-07-15 18:01:52 +0300

    feat: :sparkles: add header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+// components
+import { Button } from "primereact/button";
 
 const Header = () => {
   return (
-    <div className="header bg-white">
+    <div className="header bg-white flex align-items-center">
       <div className="container flex-between-center">
-        <div className="logo">
-          <img
-            src="https://logowik.com/content/uploads/images/135_t_letter.jpg"
-            width={70}
-          />
+        <Link to={"/"} className="logo text-4xl font-bold text-primary">
+          Tide
+        </Link>
+
+        <div className="controllers flex gap-2">
+          <Button icon="pi pi-sign-in" label="Login" size="small" />
         </div>
       </div>
     </div>
